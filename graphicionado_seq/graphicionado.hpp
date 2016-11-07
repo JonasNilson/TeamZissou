a2fde78f4b00db9956ad383dc260e507699d8441
Jonas <jonas.nilson.8705@student.uu.se>
2016-11-07 15:11:17 +0100

    Vertex prop as void pointer

--- graphicionado_seq/graphicionado.hpp
+++ graphicionado_seq/graphicionado.hpp
@@ -1,15 +1,28 @@
 // Graphicionado data types etc.
 
+/* -----------------------------------------------
+--- Graphicionado functions ----------------------
+----------------------------------------------- */
+
+/* -----------------------------------------------
+--- Graphicionado structs ------------------------
+----------------------------------------------- */
+
+// A struct representing a graph
+struct Graph {
+	void* vertexList;	// A list of all vertices
+	void* edgeList;		// A list of all edges
+}
 
 // A struct representing a vertex in the graph
 struct Vertex {
-	int Id;
-	double VertexProperty;
+	int id;
+	void* prop;
 };
 
 // A struct representing an edge in the graph
 struct Edge {
-	int SrcId;
-	int DstId;
-	int Weight;
+	int srcId;
+	int dstId;
+	int weight;
 };
