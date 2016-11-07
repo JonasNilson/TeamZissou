a70d3af12e3699422198523a22c3e32c71e7339d
warlang <agawerth@gmail.com>
2016-11-07 16:22:50 +0100

    merged main files

--- graphicionado_seq/main.cpp
+++ graphicionado_seq/main.cpp
@@ -1,9 +1,15 @@
-#include<iostream>
-#include<vector>
-#inlcude<argo.hpp>
-#include<graphiciando.hpp>
-#include<array>
+/**
+* Sequential implementation of graphicionado
+*/
 
+#include<argo.hpp> // Get access to Argo function calls.
+#include<iostream> // Used for output prints.
+#include<graphicionado.hpp> // Data structures for graph problems
+#include<array>
+#include<vector>
+
+// Global variable declaration
+int THREADS = 4; // Set number of threads
 VertexProperty VTempProperty[];
 VertexProperty VProperty[];
 VertexProperty VConst[];
@@ -11,83 +17,122 @@ Edge Edges[];
 Edge EdgeIDTable[];
 Vertex ActiveVerterx[];
 
-
 Graph graph;
 
+/**
+Information about graphicionado
+* EdgeIDTable - is constructed and stored in memory. It is an array that store edge id of the first edge of each vertex. Sorted by srcID and then dstID.
+* VProperty - is an array that store vertex property (Unknown for now)
+* VTempProperty - same as VProperty but updated from the function reduce.
+* Edges - array of all edges. Is indexed (sorted) by edge ID E.g of an edge: (srcID, dstID, weight) (NOT SURE VISULISE TOMORROW)
+*/
+is constructed and stored in memory. This array stores the eid
+of the first edge of each vertex to allow streaming accesses of
+the edges starting at a particular vertex
 
-void Process_Edge(){
 
+/* User defined prop is void pointers that gets casted*/
+// User defined computation
+/**
+* processEdge(double weight, int* srcProp, int* dstProp)
+* weight - the edge weight
+* srcProp - pointer to source property
+* dstProp - pointer to destination property
+* return: VertexProperty an 
+*/
+VertexProperty processEdge(double weight, int* srcProp, int* dstProp) {
+ //TODO: User defined computation
+ //return ...
 }
 
-void Reduce(){
-
+// User defined computation
+VertexProperty reduce(VertexProperty temp, VertexProperty result) {
+ //TODO: User defined computation
+ //return ...
 }
 
-void Apply(){
-
+// User defined computation
+VertexProperty apply(VertexProperty vprop, VertexProperty temp, VertexProperty vconst) {
+ //TODO: User defined computation
+ //return ...
 }
+
+
 
 
 int main(int argc, char *argv[]){
+	/* 
+	 Set up the argo environment, caches and global memory. 
+	 Init the total space that is shared between all nodes. 
+	*/
+	argo::init(128 * 1024 * 1024); 
 
-  argo::init(128 * 1024 * 1024);
+	// Local variable declaration
+	int id = argo::node_id(); // get this node unique index number starting from 0
+	int nodes = argo::number_of_nodes(); // return the total number of nodes in the Argo system.
 
-  Edges = graph.EdgeList;
 
-  for(int i =0; i< graph.VertexList.size()){
-    VProperty[i] = graph.VertexList[i].VertexProperty;
-  }
+	/*LINUS*/
+	Edges = graph.EdgeList;
+
+	for(int i =0; i< graph.VertexList.size()){
+    	VProperty[i] = graph.VertexList[i].VertexProperty;
+  	}
   
-  int edgeIndex=0;
+	int edgeIndex=0;
   
-  for(int i= 0; i< graph->VertexList.size(); ++i){
-    EdgeIDTable[i] = Edges[edgeIndex];
-    int j = 0;
-    while(graph->Vertex[i].id == graph->Edges[j].srcid){
-      j++;
-      edgeIndex++;
-    }
-  }
+	for(int i= 0; i< graph->VertexList.size(); ++i){
+    	EdgeIDTable[i] = Edges[edgeIndex];
+    	int j = 0;
+    	while(graph->Vertex[i].id == graph->Edges[j].srcid){
+      		j++;
+      		edgeIndex++;
+    	}
+  	}
+	ActiveVertex[0] = Vertex[0];
 
-  ActiveVertex[0] = Vertex[0];
+	/*END OF LINUS*/
 
-  
-  
-  while(1){
 
-    // Processing Phase
-    for (int i=0; i<ActiveVertexCount; i++) {
-      Vertex src = ActiveVertex[i]; // Sequential Vertex Read
-      int eid = EdgeIDTable[src.id]; // Edge ID Read
-      Edge e = Edges[eid]; // Edge Read
-      while (e.srcid == src.id) {
-	dst.prop = VProperty[e.dstid]; // [OPT IONAL] Random Vertexg Read
-	VertexProperty res = Process_Edge(e.weight, src.prop, dst.prop);
-	VertexProperty temp = VTempProperty[e.dstid]; // Random Vertex Read
-	temp = Reduce(temp, res);
-	VTempProperty[e.dstid] = temp; // Random Vertex Write
-	e = Edges[++eid]; // Edge Read
-      }
-    }
-    // Reset ActiveVertex and ActiveVertexCount
-    // Apply Phase
-    for (int i=0; i<TotalVertexCount; i++) {
-      VertexProperty vprop = VProperty[i]; // Sequential Vertex Read
-      VertexProperty temp = VTempProperty[i]; // Sequential Vertex Read
-      VertexProperty vconst = VConst[i];
-      temp = Apply(vprop, temp, vconst);
-      VProperty[i] = temp; // Sequential Vertex Write
-      if(temp != vprop) {
-	Vertex v;
-	v.id = i;
-	v.prop = temp;
-	ActiveVertex[ActiveVertexCount++] = v; // Sequential Vertex Write
-      }
-    }
 
-    
-  }
-  
-  argo::finalize();
-  
+	/* TODO: Implement section */
+	// START SUDO CODE from graphicionado
+	for (int i=0; i<ActiveVertexCount; i++) {
+		Vertex src = ActiveVertex[i]; // Sequential Vertex Read
+		int eid = EdgeIDTable[src.id]; // Edge ID Read
+		Edge e = Edges[eid]; // Edge Read
+		
+		while (e.srcid == src.id) {
+			dst.prop = VProperty[e.dstid]; // [OPT IONAL] Random Vertex Read
+			VertexProperty res = processEdge(e.weight, src.prop, dst.prop);
+			VertexProperty temp = VTempProperty[e.dstid]; // Random Vertex Read
+			temp = reduce(temp, res);
+	 		VTempProperty[e.dstid] = temp; // Random Vertex Write
+	 		e = Edges[++eid] // Edge Read
+	 	}
+	}
+	// Reset ActiveVertex and ActiveVertexCount
+	
+	//B Apply Phase
+	for (int i=0; i<TotalVertexCount; i++) {
+		VertexProperty vprop = VProperty[i]; // Sequential Vertex Read
+		VertexProperty temp = VTempProperty[i]; // Sequential Vertex Read
+		VertexProperty vconst = VConst[i];
+		temp = apply(vprop, temp, vconst);
+		VProperty[i] = temp; // Sequential Vertex Write
+		if(temp != vprop) {
+			Vertex v;
+			v.id = i;
+			v.prop = temp;
+			ActiveVertex[ActiveVertexCount++] = v; // Sequential Vertex Write
+		}
+	}
+
+	//END OF SUDO CODE
+
+
+
+	argo::finalize(); // Cleanup for this node when program has finished.
+
+	return 0;
 }
