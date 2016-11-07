5ee3ec0048bbd4e180b4741ec1ae0c5461e4b989
linush <linus.hedberg@live.se>
2016-11-07 15:10:10 +0100

    skeleton started

--- graphicionado_seq/main.cpp
+++ graphicionado_seq/main.cpp
@@ -1,30 +1,85 @@
-/**
-* Sequential implementation of graphicionado
-*/
+#include<iostream>
+#include<vector>
+#inlcude<argo.hpp>
+#include<graphiciando.hpp>
 
-#include<argo.hpp> // Get access to Argo function calls.
-#include<iostream> // Used for output prints.
+VertexProperty VTempProperty[];
+VertexProperty VProperty[];
+VertexProperty VConst[];
+Edge Edges[];
+Edge EdgeIDTable[];
+Vertex ActiveVerterx[];
 
 
-// Global variable declaration
-int THREADS = 4; // Set number of threads
+Graph graph;
+
+
+void Process_Edge(){
+
+}
+
+void Reduce(){
+
+}
+
+void Apply(){
+
+}
+
 
 int main(int argc, char *argv[]){
-	/* 
-	 Set up the argo environment, caches and global memory. 
-	 Init the total space that is shared between all nodes. 
-	*/
-	argo::init(128 * 1024 * 1024); 
 
-	// Local variable declaration
-	int id = argo::node_id(); // get this node unique index number starting from 0
-	int nodes = argo::number_of_nodes(); // return the total number of nodes in the Argo system.
+  argo::init(128 * 1024 * 1024);
 
-	//TODO: Implement section
+  Edges = graph.EdgeList;
 
+  int edgeIndex=0;
+  
+  for(int i= 0; i< graph->Vertex.lenght; ++i){
+    EdgeIDTable[i] = Edges[edgeIndex];
+    int j = 0;
+    while(graph->Vertex[i].srcid == graph->Vertex[j].srcid){
+      j++;
+      edgeIndex++;
+    }
+  }
+  
 
+  while(1){
 
-	argo::finalize(); // Cleanup for this node when program has finished.
+    // Processing Phase
+    for (int i=0; i<ActiveVertexCount; i++) {
+      Vertex src = ActiveVertex[i]; // Sequential Vertex Read
+      int eid = EdgeIDTable[src.id]; // Edge ID Read
+      Edge e = Edges[eid]; // Edge Read
+      while (e.srcid == src.id) {
+	dst.prop = VProperty[e.dstid]; // [OPT IONAL] Random Vertexg Read
+	VertexProperty res = Process_Edge(e.weight, src.prop, dst.prop);
+	VertexProperty temp = VTempProperty[e.dstid]; // Random Vertex Read
+	temp = Reduce(temp, res);
+	VTempProperty[e.dstid] = temp; // Random Vertex Write
+	e = Edges[++eid]; // Edge Read
+      }
+    }
+    // Reset ActiveVertex and ActiveVertexCount
+    // Apply Phase
+    for (int i=0; i<TotalVertexCount; i++) {
+      VertexProperty vprop = VProperty[i]; // Sequential Vertex Read
+      VertexProperty temp = VTempProperty[i]; // Sequential Vertex Read
+      VertexProperty vconst = VConst[i];
+      temp = Apply(vprop, temp, vconst);
+      VProperty[i] = temp; // Sequential Vertex Write
+      if(temp != vprop) {
+	Vertex v;
+	v.id = i;
+	v.prop = temp;
+	ActiveVertex[ActiveVertexCount++] = v; // Sequential Vertex Write
+      }
+    }
 
-	return 0;
+    
+  }
+  
+  argo::finalize();
+  
 }
