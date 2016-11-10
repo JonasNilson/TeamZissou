6f055246c3262588b27b3cb2646505845abe19d4
Jonas <jonas.nilson.8705@student.uu.se>
2016-11-10 15:56:12 +0100

    Renamed files, preprocessing extracted

--- graphicionado_seq/main.cpp
+++ graphicionado_seq/main.cpp
@@ -24,87 +24,6 @@ VertexProperty* vProperty;
 VertexProperty* vTempProperty;
 VertexProperty* vConst;
 
-/*
-* Collective allocations for the argoDSM system.
-* numVerticies: total number of verticies in graph
-* numEdges: total number of edges in graph
-*/
-void setupDSM(unsigned int numVerticies, unsigned int numEdges){
-	verticies = argo::conew_array<Vertex>(numVerticies); 
-	activeVertex = argo::conew_array<Vertex>(numVerticies); 
-	
-	edges = argo::conew_array<Edge>(numEdges); 
-	edgeIDTable = argo::conew_array<unsigned int>(numVerticies); // make it of size number of verticies
-
-	vProperty = argo::conew_array<VertexProperty>(numVerticies);
-	vTempProperty = argo::conew_array<VertexProperty>(numVerticies);
-	vConst = argo::conew_array<VertexProperty>(numVerticies);
-}
-
-
-/*
-* Compare edges with srcID then dstID
-*/
-bool edgeCompare(edge e1, edge e2){
-	// Sort by srcID
-	if(e1.srcID < e2.srcID || (e1.srcID == e2.srcID && e1.dstID < e2.dstID)){
-		return true;
-	}
-	return false;
-}
-
-/*
-* Compare vertex with ID
-*/
-bool vertexCompare(vertex v1, vertex v2){
-	if(v1.ID < v2.ID){
-		return true;
-	}
-	return false;
-}
-
-
-/*
-* Organize already collective allocated space in the argoDSM system 
-* with data like graphicionado's model.
-* numVerticies: total number of verticies in graph
-* numEdges: total number of edges in graph
-*/
-void initializeDSM(unsigned int numVerticies, unsigned int numEdges){
-	
-	// Sort edges after srcID and then dstID with function edgeCompare
-	std::sort(edges,edges[numEdges],edgeCompare); // if this one does not work use std::sort(edges,edges+numEdges,edgeCompare);
-
-	// Sort verticies after ID to make sure the edge ID table correspond to correct node.
-	std::sort(verticies,verticies[numVerticies],vertexCompare);
-
-  	// TODO: Init ActiveVerticies
-	// root node? where we start with. Maybe an argument what we take in
-
-  	// Init EdgeIDTable
-  	unsigned int vertex_ID = 0;
-	for(unsigned int i = 0; i < numEdges; ++i){
-		if(edges[i].srcID == vertex_ID){
-			edgeIDTable[vertex_ID] = i;
-			vertex_ID++;
-		}
-	}
-
-  	// Init VProperty
-	for(unsigned int i =0; i < numVerticies; ++i) {
-    	vProperty[i] = verticies[i].prop;
-  	}
-
-  	// TODO: Init VTempProperty
-  	//WARNING might not be needed to initialized 
-  	for(unsigned int i =0; i < numVerticies; ++i) {
-      	vProperty[i] = verticies[i].prop;
-   	}
-
-  	// TODO: Init VConst
-   	// Get more info what this is? Do we get this from the data?
-
-}
 /**
 Information about graphicionado
 * EdgeIDTable - is constructed and stored in memory. It is an array that store edge id of the first edge of each vertex. Sorted by srcID and then dstID.
@@ -112,11 +31,6 @@ Information about graphicionado
 * VTempProperty - same as VProperty but updated from the function reduce.
 * Edges - array of all edges. Is indexed (sorted) by edge ID E.g of an edge: (srcID, dstID, weight) (NOT SURE VISULISE TOMORROW)
 */
-
-
-
-
-
 
 int main(int argc, char *argv[]){
 	/* 
