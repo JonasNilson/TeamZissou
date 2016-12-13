53749ee5551ebaca89d47668ab6597c26fea5f61
Jonas <jonas.nilson.8705@student.uu.se>
2016-12-13 11:55:04 +0100

    Unnecessary print removed

--- graphicionado_par/preprocess.cpp
+++ graphicionado_par/preprocess.cpp
@@ -366,7 +366,6 @@ void readHeader(const char* filename) {
 	}
 
 	// Read the line starting with the character 'p' containing info. about number of vert/edges
-	std::cout << "comp borde vara p: " << comp << std::endl;
 	if(comp == 'p'){
 		std::stringstream ss; // Create a new string stream
 		ss.str(line);
@@ -412,7 +411,6 @@ void readData(const char* filename) {
 	}
 
 	// Read the line starting with the character 'p' containing info. about number of vert/edges
-	std::cout << "comp borde vara p: " << comp << std::endl;
 	if(comp == 'p'){
 		std::stringstream ss; // Create a new string stream
 		ss.str(line);
@@ -563,8 +561,6 @@ void setupEIT(unsigned int numVertices, unsigned int numEdges, unsigned int* edg
 * Calculate the number of edges for each stream. 
 */
 void readNumEdgesFromFile(const char* filename) {
-	std::cout << "@ readNumEdgesFromFile: node id: " << argo::node_id() << std::endl; 
-
 	//Read file
 	std::ifstream file;
 	std::string line;
